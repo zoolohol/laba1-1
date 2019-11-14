671cc8ced01248f43b597591b28c759765368467
LinkIvan333 <zamotaev.ivan@list.ru>
2019-11-14 20:15:50 +0300

    Задание 1

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/Git/lab-01-parser
+# Build directory: /cygdrive/c/Git/lab-01-parser/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(unit_tests "/cygdrive/c/Git/lab-01-parser/cmake-build-debug/tests.exe")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/cygdrive/c/Git/lab-01-parser/CMakeLists.txt;47;add_test;/cygdrive/c/Git/lab-01-parser/CMakeLists.txt;0;")
---
+++ include/Json.hpp
@@ -0,0 +1,64 @@
+#ifndef UNTITLED_JSON_H
+#define UNTITLED_JSON_H
+
+#include <any>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <typeinfo>
+#include <fstream>
+
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::any;
+using std::vector;
+using std::map;
+using std::any_cast;
+
+class Json {
+private:
+    map <string, any> json_map;
+    vector <any> json_arr;
+
+    bool is_json_array = false;
+    bool is_json_object = false;
+
+    void create_vector(const string& s);
+    void create_map(const string& s);
+
+public:
+
+    Json(const string& s);
+
+    bool is_array() const;
+
+    bool is_object() const;
+
+    std::any& operator[](const string& key);
+
+    std::any& operator[](int index);
+
+    static Json parse(const string& s);
+
+    static Json parseFile(const string& path_to_file);
+
+    static unsigned int skip_spaces(unsigned int i, const string& s);
+
+    static string read_word(unsigned int &i, const string& s);
+
+    static unsigned int find_end(unsigned int i, const string& s);
+
+    static string cut_num(unsigned int i, const string& s);
+
+    void print_map();
+
+    void print_vector();
+
+    static void print(any _data);
+
+};
+
+#endif //UNTITLED_JSON_H
