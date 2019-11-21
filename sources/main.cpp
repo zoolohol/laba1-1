645d9e74401cdbcea860544f959715b81b4e09ba
LinkIvan333 <zamotaev.ivan@list.ru>
2019-11-21 17:13:22 +0300

    unit

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,22 +1,17 @@
-//
-// Created by LinkIvan333 on 14.11.2019.
-//
-
 #include <iostream>
 #include <fstream>
 #include <nlohmann/json.hpp>
 
-using namespace std;
 using json = nlohmann::json;
 
 int main() {
-    ifstream in("C:/Git/lab-01-parser/file.json");
+    std::ifstream in("C:/Git/lab-01-parser/file.json");
     json obj = json::parse(in);
     in.close();
     auto arr1 = obj[0];
     auto arr2 = obj[1];
     auto arr3 = obj[2];
-    ofstream o("file2.json");
+    std::ofstream o("file2.json");
     o << "[\n";
     for(int i = 0; i < 3; i++) {
         o << "    "  << "{ \"ticker\" : " << arr1[i] << ", \"id\": " << arr2[i] <<", \"description\": " << arr3[i] << "}";
@@ -25,9 +20,9 @@ int main() {
     }
     o << "]\n";
     o.close();
-    ifstream in1("file2.json");
+    std::ifstream in1("file2.json");
     json new_obj;
     in1 >> new_obj;
     in1.close();
-    cout << new_obj.dump(5);
+    std::cout << new_obj.dump(5);
 }
