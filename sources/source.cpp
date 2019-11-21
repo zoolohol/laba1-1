aae6b3de54c6b26a4b2e8373aa7f16c6e42a7f9f
LinkIvan333 <zamotaev.ivan@list.ru>
2019-11-21 22:33:59 +0300

    unit

--- sources/source.cpp
+++ sources/source.cpp
@@ -5,10 +5,10 @@
 int main() {
         string text;
         text = R"({ "lastname" : "Ivanov" , "firstname" : "Ivan" , "age" : 25,"islegal" : false,"marks" : [ 4 , 5 , 5 , 5 , 2 , 3 ],"address" : {  "city" : "Moscow" ,  "street" : "Vozdvijenka" } })";
-	      Json object = Json::parse(text);
-	      cout << std::endl;
-	      if (object.is_object())
-	          cout << "Yes";
+        Json object = Json::parse(text);
+        cout << std::endl;
+        if (object.is_object())
+            cout << "Yes";
 
     return 0;
 }
